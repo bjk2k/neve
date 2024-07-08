3bac5c5799ae93e77889a6b04fa221508959eb86
Ben-Jasper Kettlitz <b-jk@gmx.net>
2024-07-08 16:57:53 +0200

    Add novel class

--- config/languages/prose.nix
+++ config/languages/prose.nix
@@ -1,6 +1,10 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive.combine) scheme-full tocbibind novel;
+  };
+in {
   plugins.vimtex = {
     enable = true;
-    texlivePackage = pkgs.texlive.combined.scheme-full;
+    texlivePackage = tex;
   };
 }

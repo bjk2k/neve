b5f4c7293d5f3d4db243b65358df63703544c861
Ben-Jasper Kettlitz <b-jk@gmx.net>
2024-07-08 17:03:55 +0200

    Remove latex installation

--- config/languages/prose.nix
+++ config/languages/prose.nix
@@ -1,10 +1,6 @@
-{pkgs, ...}: let
-  tex = pkgs.texlive.combine {
-    inherit (pkgs.texlive.combine) scheme-full tocbibind novel;
-  };
-in {
+{
   plugins.vimtex = {
     enable = true;
-    texlivePackage = tex;
+    texlivePackage = null;
   };
 }

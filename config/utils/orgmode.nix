28253d13c9d10befde047c77bde4f415f05c4086
Ben-Jasper Kettlitz <b-jk@gmx.net>
2024-09-07 21:37:39 +0200

    Add orgmode specification.

--- config/utils/orgmode.nix
+++ config/utils/orgmode.nix
@@ -1,5 +1,9 @@
 {
   plugins.orgmode = {
     enable = true;
+    settings = {
+      org_agenda_files = "~/personal/orgfiles/*";
+      org_default_notes_file = "~/personal/orgfiles/notes.org";
+    };
   };
 }

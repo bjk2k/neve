11f50aed19da6a99cf41db1221ce020b1291e8fd
Ben-Jasper Kettlitz <b-jk@gmx.net>
2024-09-07 21:44:22 +0200

    Fix orgmode location.

--- config/utils/orgmode.nix
+++ config/utils/orgmode.nix
@@ -2,7 +2,7 @@
   plugins.orgmode = {
     enable = true;
     settings = {
-      org_agenda_files = "~/personal/orgfiles/*";
+      org_agenda_files = "~/personal/orgfiles/**/*";
       org_default_notes_file = "~/personal/orgfiles/notes.org";
     };
   };

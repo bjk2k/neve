0a3562e640189b9c0c6c6a78e750c4cda50918ff
Ben-Jasper Kettlitz <b-jk@gmx.net>
2024-12-05 00:08:33 +0100

    Fix theme.

--- config/colorschemes/kanagawa.nix
+++ config/colorschemes/kanagawa.nix
@@ -1,16 +1,21 @@
-{ lib, config, ... }:
-{
+{ lib, config, ... }: {
   options = {
     kanagawa.enable = lib.mkEnableOption "Enable kanagawa Colorscheme module";
+
   };
   config = lib.mkIf config.kanagawa.enable {
     colorschemes = {
-    kanagawa = {
-      enable = true;
-      settings = {
-        theme = "dragon";
+      kanagawa = {
+        enable = true;
+        settings = {
+          theme = "dragon";
+          background = {
+            dark = "dragon";
+            light = "wave";
+          };
+        };
       };
     };
   };
-  };
 }
+

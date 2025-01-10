970201137ea331ca8ff9241ff4440235c2da1b76
Ben-Jasper Kettlitz <b-jk@gmx.net>
2025-01-10 22:13:23 +0100

    Nvterm fix.

--- config/utils/nvterm.nix
+++ config/utils/nvterm.nix
@@ -39,8 +39,8 @@
       		function()
       			terminal.toggle("horizontal")
       		end,
-      	},
-      	      }
+      	}
+      }
       local opts = { noremap = true, silent = true }
       for _, mapping in ipairs(mappings) do
       	vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)

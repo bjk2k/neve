62c225fcd207e9b629c6629344e1c237c21c4666
Ben-Jasper Kettlitz <b-jk@gmx.net>
2025-11-02 00:23:53 +0100

    Fix stuff

--- config/languages/treesitter-nvim.nix
+++ config/languages/treesitter-nvim.nix
@@ -1,4 +1,5 @@
-{ lib, config, ... }: {
+{ lib, config, ... }:
+{
   options = {
     treesitter-nvim.enable = lib.mkEnableOption "Enable treesitter-nvim module";
   };
@@ -13,10 +14,18 @@
           "DeferredUIEnter"
         ];
         settings = {
-          highlight = { enable = true; };
-          indent = { enable = true; };
-          autopairs = { enable = true; };
-          folding = { enable = true; };
+          highlight = {
+            enable = true;
+          };
+          indent = {
+            enable = true;
+          };
+          autopairs = {
+            enable = true;
+          };
+          folding = {
+            enable = true;
+          };
           ensure_installed = [
             "bash"
             "c"
@@ -60,53 +69,61 @@
 
       treesitter-textobjects = {
         enable = true;
-
-        select = {
-          enable = true;
-          lookahead = true;
-          keymaps = {
-            "aa" = "@parameter.outer";
-            "ia" = "@parameter.inner";
-            "af" = "@function.outer";
-            "if" = "@function.inner";
-            "ac" = "@class.outer";
-            "ic" = "@class.inner";
-            "ii" = "@conditional.inner";
-            "ai" = "@conditional.outer";
-            "il" = "@loop.inner";
-            "al" = "@loop.outer";
-            "at" = "@comment.outer";
+        settings = {
+          select = {
+            enable = true;
+            lookahead = true;
+            keymaps = {
+              "aa" = "@parameter.outer";
+              "ia" = "@parameter.inner";
+              "af" = "@function.outer";
+              "if" = "@function.inner";
+              "ac" = "@class.outer";
+              "ic" = "@class.inner";
+              "ii" = "@conditional.inner";
+              "ai" = "@conditional.outer";
+              "il" = "@loop.inner";
+              "al" = "@loop.outer";
+              "at" = "@comment.outer";
+            };
           };
-        };
-        move = {
-          enable = true;
-          gotoNextStart = {
-            "]m" = "@function.outer";
-            "]]" = "@class.outer";
+          move = {
+            enable = true;
+            goto_next_start = {
+              "]m" = "@function.outer";
+              "]]" = "@class.outer";
+            };
+            goto_next_end = {
+              "]M" = "@function.outer";
+              "][" = "@class.outer";
+            };
+            goto_previous_start = {
+              "[m" = "@function.outer";
+              "[[" = "@class.outer";
+            };
+            goto_previous_end = {
+              "[M" = "@function.outer";
+              "[]" = "@class.outer";
+            };
           };
-          gotoNextEnd = {
-            "]M" = "@function.outer";
-            "][" = "@class.outer";
+          swap = {
+            enable = true;
+            swap_next = {
+              "<leader>a" = "@parameters.inner";
+            };
+            swap_previous = {
+              "<leader>A" = "@parameter.outer";
+            };
           };
-          gotoPreviousStart = {
-            "[m" = "@function.outer";
-            "[[" = "@class.outer";
-          };
-          gotoPreviousEnd = {
-            "[M" = "@function.outer";
-            "[]" = "@class.outer";
-          };
-        };
-        swap = {
-          enable = true;
-          swapNext = { "<leader>a" = "@parameters.inner"; };
-          swapPrevious = { "<leader>A" = "@parameter.outer"; };
         };
       };
 
-      ts-autotag = { enable = true; };
+      ts-autotag = {
+        enable = true;
+      };
 
-      treesitter-context = { enable = false; 
+      treesitter-context = {
+        enable = false;
         lazyLoad.settings.event = [
           "BufNewFile"
           "BufReadPost"
@@ -118,7 +135,7 @@
 
       ts-context-commentstring = {
         enable = true;
-        
+
         disableAutoInitialization = false;
       };
     };
